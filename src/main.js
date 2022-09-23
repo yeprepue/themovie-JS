fd93d889e120d70de0eba20165ebe31f4eb2a836
Yeison Esteban Pretel Puentes <43614257+yeprepue@users.noreply.github.com>
2022-09-22 19:56:19 -0500

    Se agrega las peliculas en tendencia desde el DOM

--- src/main.js
+++ src/main.js
@@ -2,17 +2,20 @@ async function getTrendingMoviesPreview ( ) {
     const res = await fetch('https://api.themoviedb.org/3/trending/movie/day?api_key='+ API_KEY);
     const data = await res.json();
 
-    const movies = data.results;
-    console.log({data, movies}); 
+    const movies = data.results; 
+    movies.forEach(movie => {
+        const trendingPreviewMoviesContainer = document.querySelector('#trendingPreview .trendingPreview-movieList')
 
-    movies.array.forEach(movie => {
         const movieContainer = document.createElement('div');
         movieContainer.classList.add('movie-container');
 
         const movieImg = document.createElement('img');
         movieImg.classList.add('movie-img');
         movieImg.setAttribute('alt', movie.title);
-        movieImg.setAttribute('src', 'https://image.tmdb.org/t/p/w300/' + poster_path,); 
+        movieImg.setAttribute('src', 'https://image.tmdb.org/t/p/w300/' + movie.poster_path,); 
+
+        movieContainer.appendChild(movieImg);
+        trendingPreviewMoviesContainer.appendChild(movieContainer);
     });
 }
 
