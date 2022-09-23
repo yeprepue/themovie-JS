2a54676f6169b95ce816834a6d2fd053eb0dd49d
Yeison Esteban Pretel Puentes <43614257+yeprepue@users.noreply.github.com>
2022-09-22 19:19:30 -0500

    Se agrega llamada a la API con fecth

--- src/main.js
+++ src/main.js
@@ -1,1 +1,19 @@
-console.log('hello world')
+async function getTrendingMoviesPreview ( ) {
+    const res = await fetch('https://api.themoviedb.org/3/trending/movie/day?api_key='+ API_KEY);
+    const data = await res.json();
+
+    const movies = data.results;
+    console.log({data, movies}); 
+
+    movies.array.forEach(movie => {
+        const movieContainer = document.createElement('div');
+        movieContainer.classList.add('movie-container');
+
+        const movieImg = document.createElement('img');
+        movieImg.classList.add('movie-img');
+        movieImg.setAttribute('alt', movie.title);
+        movieImg.setAttribute('src', 'https://image.tmdb.org/t/p/w300/' + poster_path,); 
+    });
+}
+
+getTrendingMoviesPreview ();
