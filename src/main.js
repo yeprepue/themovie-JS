1061849f7d298d0f6d2178d8d92fc1b69929a256
Yeison Esteban Pretel Puentes <43614257+yeprepue@users.noreply.github.com>
2022-09-22 23:23:25 -0500

    Se agrega lista de categorias y lb axios

--- src/main.js
+++ src/main.js
@@ -1,8 +1,16 @@
+const api = axios.create({
+    baseURL: 'https://api.themoviedb.org/3/',
+    headers:{
+        'Content-Type': 'aplication/json;charset=utf-8',
+    },
+    params: {
+        'api_key': API_KEY,
+    }
+});
 async function getTrendingMoviesPreview ( ) {
-    const res = await fetch('https://api.themoviedb.org/3/trending/movie/day?api_key='+ API_KEY);
-    const data = await res.json();
-
+    const { data } = await api('trending/movie/day');
     const movies = data.results; 
+
     movies.forEach(movie => {
         const trendingPreviewMoviesContainer = document.querySelector('#trendingPreview .trendingPreview-movieList')
 
@@ -18,5 +26,30 @@ async function getTrendingMoviesPreview ( ) {
         trendingPreviewMoviesContainer.appendChild(movieContainer);
     });
 }
+async function  getCategoriesPreview() {
+    const { data } = await api('genre/movie/list');
+    const categories = data.genres; 
 
+    categories.forEach(category =>{
+        const previewCategoriesContainer = document.querySelector('#categoriesPreview .categoriesPreview-list');
+
+        const categoryContainer = document.createElement('div');
+        categoryContainer.classList.add('category-container');
+
+        const categoryTitle =  document.createElement('h3');
+        categoryTitle.classList.add('category-title');
+
+        categoryTitle.setAttribute('id', 'id' + category.id);
+
+        const categoryTitleText = document.createTextNode(category.name);
+
+        categoryTitle.appendChild(categoryTitleText);
+        categoryContainer.appendChild(categoryTitle);
+        previewCategoriesContainer.appendChild(categoryContainer);
+
+    });
+
+
+}
 getTrendingMoviesPreview ();
+getCategoriesPreview();
